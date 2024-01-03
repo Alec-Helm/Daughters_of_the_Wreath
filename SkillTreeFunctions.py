9c8079c1a01350c3d6181a8f97c1b6f376318383
Alec Helm <141180412+Alec-Helm@users.noreply.github.com>
2024-01-03 03:11:31 -0500

    Update SkillTreeFunctions.py

--- SkillTreeFunctions.py
+++ SkillTreeFunctions.py
@@ -17,8 +17,8 @@ def nodeCompile(csv_path):
         for row in csv_reader:
             if line_count == 0:
                 line_count += 1
-            else:
-                next_node_dict = {"Image" : row[1], "Color" : 'Red', "Angle" : int(math.floor(float(row[2]))), "Radius" : int(row[3]), "Size" : int(row[4]), "ID" : row[5], "Neighbors" : None, "X" : None, "Y" : None, "Surface": None, "Rectangle": None, "State" : 0, "Accessible" : 0}
+            elif row[0] != '':
+                next_node_dict = {"Index" : str(row[0]), "Image" : row[1], "Color" : 'Red', "Angle" : int(math.floor(float(row[2]))), "Radius" : int(row[3]), "Size" : int(row[4]), "ID" : row[5], "Neighbors" : None, "X" : None, "Y" : None, "Surface": None, "Rectangle": None, "State" : 0, "Accessible" : 0}
                 neighbor_list = []
                 for neighbor_index in [6,7,8,9]:
                     next_neighbor = row[neighbor_index]
@@ -66,52 +66,61 @@ def characterCompile():
 #FUNCTIONS THAT RUN TO IMPLEMENT PLAYER INPUT
 #
 #
+colors = {"On" : (32,11,166), "Accessible" : (127, 16, 135)}
+
 def left_click(highlighted_node, node_dict, node_effects, character, first_node):
     if highlighted_node != 0:
         if node_dict[str(highlighted_node)]["Accessible"] == 1 and node_dict[str(highlighted_node)]["State"] == 0:
             character[0]["Level"] += 1
+            tree = str(highlighted_node)[0]
+            first_time = first_node[str(tree)]
+            first_node[str(tree)] = False
 
-            if not first_node:
+            if not first_time:
                 #we need to turn the node on, and make all its off-neighbor's accessible
                 node_dict[str(highlighted_node)]["State"] = 1
-                node_dict[str(highlighted_node)]["Color"] = (32,11,166)
-                node_dict[str(highlighted_node)]["Surface"].fill((32,11,166))
+                node_dict[str(highlighted_node)]["Color"] = colors["On"]
+                node_dict[str(highlighted_node)]["Surface"].fill(colors["On"])
 
                 for neighbor in node_dict[str(highlighted_node)]["Neighbors"]:
                     if node_dict[str(neighbor)]["State"] == 0:
                         node_dict[str(neighbor)]["Accessible"] = 1
-                        node_dict[str(neighbor)]["Color"] = (127, 16, 135)
-                        node_dict[str(neighbor)]["Surface"].fill((127, 16, 135))
+                        node_dict[str(neighbor)]["Color"] = colors["Accessible"]
+                        node_dict[str(neighbor)]["Surface"].fill(colors["Accessible"])
                 
                 #then we need to implement the effect into the character
                 #grab the function name from the effect document and run it
                 run_string = node_effects[node_dict[highlighted_node]["ID"]]["Function"] + "(character)"
                 character = eval(run_string)
 
-                return [node_dict, character, False]
+                return [node_dict, character, first_node]
 
                 
             else:
-                for starter in [101,102,103]:
-                    if highlighted_node != starter:
-                        node_dict[str(starter)]["Accessible"] = 0
-                        node_dict[str(starter)]["Color"] = 'Red'
-                        node_dict[str(starter)]["Surface"].fill('Red')
+                for node in node_dict:
+                    #iterate only through starter nodes
+                    if len(node_dict[str(node)]["Index"]) == 3:
+                        #only include starter nodes of the proper tree
+                        if str(node)[0] == tree:
+                            #then deal with every starter node EXCEPT the highlighted one
+                            if highlighted_node != node:
+                                node_dict[str(node)]["Accessible"] = 0
+                                node_dict[str(node)]["Color"] = 'Red'
+                                node_dict[str(node)]["Surface"].fill('Red')                        
 
-                    node_dict[str(highlighted_node)]["State"] = 1
-                    node_dict[str(highlighted_node)]["Color"] = (32,11,166)
-                    node_dict[str(highlighted_node)]["Surface"].fill((32,11,166))
+                node_dict[str(highlighted_node)]["State"] = 1
+                node_dict[str(highlighted_node)]["Color"] = colors["On"]
+                node_dict[str(highlighted_node)]["Surface"].fill(colors["On"])
 
-                    for neighbor in node_dict[str(highlighted_node)]["Neighbors"]:
-                        if node_dict[str(neighbor)]["State"] == 0:
-                            node_dict[str(neighbor)]["Accessible"] = 1
-                            node_dict[str(neighbor)]["Color"] = (127, 16, 135)
-                            node_dict[str(neighbor)]["Surface"].fill((127, 16, 135))
+                for neighbor in node_dict[str(highlighted_node)]["Neighbors"]:
+                    if node_dict[str(neighbor)]["State"] == 0:
+                        node_dict[str(neighbor)]["Accessible"] = 1
+                        node_dict[str(neighbor)]["Color"] = colors["Accessible"]
+                        node_dict[str(neighbor)]["Surface"].fill(colors["Accessible"])
 
                 run_string = node_effects[node_dict[highlighted_node]["ID"]]["Function"] + "(character)"
                 character = eval(run_string)
 
-                first_node = False
                 return [node_dict, character, first_node]
         else:
             return [node_dict, character, first_node]
@@ -127,10 +136,10 @@ def left_click(highlighted_node, node_dict, node_effects, character, first_node)
 
 
 #returns a new list of all node locations of form [node_index, node_neighbors, [x-coor, y-coor]]
-def add_carts(center_point, dict):
+def add_carts(centers, dict):
     for node in dict:
-        dict[str(node)]["X"] = center_point[0] + dict[str(node)]["Radius"] * math.cos((dict[str(node)]["Angle"])*math.pi/180)
-        dict[str(node)]["Y"] = center_point[1] - dict[str(node)]["Radius"] * math.sin((dict[str(node)]["Angle"])*math.pi/180)    
+        dict[str(node)]["X"] = centers[dict[str(node)]["Index"][0]][0] + dict[str(node)]["Radius"] * math.cos((dict[str(node)]["Angle"])*math.pi/180)
+        dict[str(node)]["Y"] = centers[dict[str(node)]["Index"][0]][1] - dict[str(node)]["Radius"] * math.sin((dict[str(node)]["Angle"])*math.pi/180)    
     return dict
 
 
@@ -147,22 +156,24 @@ def adjacent_node_lines(dict, screen):
 
 #finds the radial locations of each node given a new centerpoint
 def find_radials(new_center, dict):
-    temp_dict = dict
+    temp_dict = {}
     for node in dict:
         delta_x = new_center[0] - dict[str(node)]["X"]
         delta_y = new_center[1] - dict[str(node)]["Y"]
-        temp_dict[str(node)]["Angle"] = math.atan2(delta_x,delta_y) + math.pi
-        temp_dict[str(node)]["Radius"] = math.sqrt(delta_x*delta_x + delta_y*delta_y)
+        next_dict = {"Angle": math.atan2(delta_x,delta_y) + math.pi/2, "Radius" : math.sqrt(delta_x*delta_x + delta_y*delta_y)}
+        temp_dict.update({str(node) : next_dict})
     return temp_dict
 
 #finds the cartesian locations after scaling
 def find_carts(center, temp_dict):
+    cart_dict = {}
     for node in temp_dict:
-        temp_dict[str(node)]["X"] = center[0] + temp_dict[str(node)]["Radius"] * math.cos((temp_dict[str(node)]["Angle"]))
-        temp_dict[str(node)]["Y"] = center[1] - temp_dict[str(node)]["Radius"] * math.sin((temp_dict[str(node)]["Angle"]))
+        X = center[0] + temp_dict[str(node)]["Radius"] * math.cos((temp_dict[str(node)]["Angle"]))
+        Y = center[1] - temp_dict[str(node)]["Radius"] * math.sin((temp_dict[str(node)]["Angle"]))
+        next_dict = {"X" : X, "Y": Y}
 
-        temp_dict[str(node)]["Rectangle"] = temp_dict[str(node)]["Surface"].get_rect(center = [temp_dict[str(node)]["X"],temp_dict[str(node)]["Y"]])
-    return temp_dict
+        cart_dict.update({str(node): next_dict})
+    return cart_dict
 
 
 
@@ -204,12 +215,3 @@ def calc_stats(character):
 
 
 
-def save_character(dict, char):
-    #save the current state into a .txt file
-    save_file = open("character_nodes.txt", "w")
-    for node in dict:
-        save_file.write(dict[node]["Accessible"] + "\n")
-        save_file.write(dict[node]["State"] + "\n")
-    
-    save_file = open("character_stats.txt", "w")
-    for 
